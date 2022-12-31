c945ce3cf34e3c208fce47866502743b297e4b4b
sh-vasily <vasia.punk.rok@gmail.com>
2022-12-31 11:25:57 +0300

    Парсинг файла из ресурсов, чтобы не зависеть от полного пути

--- noslq-master/src/main/kotlin/mai/dep810/noslq/Conf/MongoParser.kt
+++ noslq-master/src/main/kotlin/mai/dep810/noslq/Conf/MongoParser.kt
@@ -1,21 +1,17 @@
 package mai.dep810.noslq.Conf
 
 import mai.dep810.noslq.model.Data
-import java.io.File
-import java.io.FileInputStream
 
-class MongoParser {}
-   fun inp(){
-       val inputStream =
-           FileInputStream(File("C:\\Users\\Varvara\\Desktop\\noslq-master\\noslq-master\\src\\main\\kotlin\\mai\\dep810\\noslq\\database\\users.csv"))
-       val clients = Data.readCsv(inputStream)
-       clients.forEach {
-           println(it)
-       }
-           /*	val inputStr = FileInputStream(File("D:\\download\\data\\books.csv"))
-                   val books= Data.readC((inputStr))
-                   books.forEach {
-                       println(it)
-                   }*/
+fun inp() {
+    val usersStream = {}.javaClass.classLoader.getResourceAsStream("users.csv")
+    val clients = Data.readCsv(usersStream)
+    clients.forEach {
+        println(it)
+    }
+    /*	val inputStr = FileInputStream(File("D:\\download\\data\\books.csv"))
+            val books= Data.readC((inputStr))
+            books.forEach {
+                println(it)
+            }*/
 
-   }
+}
