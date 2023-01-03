e4686361ac0016ba6bdf42fa27a92081888833bb
Kleo <57259336+erasdron@users.noreply.github.com>
2023-01-03 20:16:10 +0300

    Add files via upload

--- noslq-master/src/main/kotlin/mai/dep810/noslq/NoslqApplication.kt
+++ noslq-master/src/main/kotlin/mai/dep810/noslq/NoslqApplication.kt
@@ -5,30 +5,16 @@ import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoDatabase
 import mai.dep810.noslq.Conf.Mongoconnect.mongo
-import mai.dep810.noslq.Conf.inp
 import mai.dep810.noslq.model.Data
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+import java.io.File
+import java.io.FileInputStream
 
 @SpringBootApplication
 class NoslqApplication
-	fun main(args: Array<String>) {
-		runApplication<NoslqApplication>(*args)
-		inp()
-		mongo()
 
-//		var mongoClient: MongoClient?=null
-//		val database: MongoDatabase
-//		val mongourl="mongodb://localhost:27017/"
-//		try{
-//			mongoClient= MongoClients.create(mongourl)
-//			database= mongoClient.getDatabase("airbnb")
-//
-//		} catch (e: MongoException) {
-//			e.printStackTrace()
-//		} finally {
-//			mongoClient!!.close()
-//		}
-
+		fun main(args: Array<String>) {
+			runApplication<NoslqApplication>(*args)
+			mongo()
 	}
