2e013f8889454fe7879d6c286c5cd764cae4279d
sh-vasily <vasia.punk.rok@gmail.com>
2023-01-03 12:00:27 +0300

    фикс репозитория для комнат

--- noslq-master/src/main/kotlin/mai/dep810/noslq/api/RoomController.kt
+++ noslq-master/src/main/kotlin/mai/dep810/noslq/api/RoomController.kt
@@ -1,51 +1,20 @@
 package mai.dep810.noslq.api
-import mai.dep810.noslq.model.Data.Room
-import mai.dep810.noslq.rep.Roomrepository
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import mai.dep810.noslq.model.Documents
+import mai.dep810.noslq.rep.RoomRepository
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
-class RoomController(val roomrepository: Roomrepository) {
+class RoomController(val roomRepository: RoomRepository) {
     @GetMapping("/api/room/find")
-    fun findClients(): List<Room> =roomrepository.findAll();
+    fun findClients(): MutableList<Documents.Rooms> = roomRepository.findAll()
 
     @GetMapping("api/room/{id}")
-    fun findClientsid(@PathVariable id:String): Room?=roomrepository.findById(id);
+    fun findClientsid(@PathVariable id:String): Optional<Documents.Rooms> = roomRepository.findById(id)
 
-    @PostMapping("api/room/add")
-    fun addRoom(@RequestBody room: Room) = roomrepository.addRoom(room);
+    @PostMapping("api/room")
+    fun addRoom(@RequestBody room: Documents.Rooms) = roomRepository.insert(room)
 
-    @DeleteMapping("api/room/delete/{id}")
-    fun deleteRoom(@PathVariable id: String)=roomrepository.deleteRoom(id);
-
-    /*
-    @RequestMapping("/api/rooms")
-
-    class RoomController {
-        @Autowired private lateinit var roomService: RoomService
-
-        @GetMapping("api/room/all")
-       fun findAll(): List<Rooms> = roomService.findAll()
-
-       @GetMapping("/{id}")
-        fun findById(@PathVariable("id") id: String):
-                Room {
-            return roomService.findById(id)
-        }
-
-
-       @PostMapping
-        fun createRoom(@Valid @RequestBody request: RoomRequest) =roomService.create(request)
-
-
-        @DeleteMapping("/{id}")
-        fun deleteRoom(@PathVariable id: String)=roomService.deleteById(id);
-
-    */
-
-
+    @DeleteMapping("api/room/{id}")
+    fun deleteRoom(@PathVariable id: String) = roomRepository.deleteById(id)
 }
--- noslq-master/src/main/kotlin/mai/dep810/noslq/rep/Roomrepository.kt
+++ noslq-master/src/main/kotlin/mai/dep810/noslq/rep/Roomrepository.kt
@@ -1,39 +1,8 @@
 package mai.dep810.noslq.rep
-import mai.dep810.noslq.model.Data
-import mai.dep810.noslq.model.Data.Room
+
 import mai.dep810.noslq.model.Documents
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
-
-
-
-
-@Repository("roomrepository")
-/*public interface Roomrepository : MongoRepository<Documents.Rooms, String> {
-    fun findAllRooms(): List<Documents.Rooms>
-    *//*fun deleteRoom(room_name: String)=rooms.removeIf{it.room_name==room_name}*//*
-
-}*/
-
-
-
-class Roomrepository {
-
-    private val rooms: MutableList<Room> = mutableListOf(
-
-    )
-
-    fun findAll(): List<Room> = rooms;
-    fun findById(room_id : String): Room?=rooms.singleOrNull{it.room_id==room_id}
-
-    fun addRoom(room: Room){
-        if(!rooms.contains(room)){
-            rooms.add(room)
-        }
-    }
-
-    fun deleteRoom(room_id: String)=rooms.removeIf{it.room_id==room_id}
-
-}
-
+@Repository
+interface RoomRepository: MongoRepository<Documents.Rooms, String>
